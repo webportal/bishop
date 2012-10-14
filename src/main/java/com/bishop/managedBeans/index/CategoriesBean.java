0dcbf0e455fe56e6a5dff5fde5db86d28429724b
shevchik <berrouz@gmail.com>
2012-10-14 20:30:54 +0200

    mockups updates

---
+++ src/main/java/com/bishop/managedBeans/index/CategoriesBean.java
@@ -0,0 +1,15 @@
+package com.bishop.managedBeans.index;
+
+import javax.inject.Named;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shevchik
+ * Date: 14.10.12
+ * Time: 19:53
+ * To change this template use File | Settings | File Templates.
+ */
+@Named
+public class CategoriesBean {
+
+}
