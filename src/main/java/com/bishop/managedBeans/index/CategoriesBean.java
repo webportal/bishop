b971fdee327a644f53ba8b291b509a8235660fd2
shevchik <berrouz@gmail.com>
2012-10-14 22:11:58 +0200

    menu on the index page is working

--- src/main/java/com/bishop/managedBeans/index/CategoriesBean.java
+++ src/main/java/com/bishop/managedBeans/index/CategoriesBean.java
@@ -1,6 +1,9 @@
 package com.bishop.managedBeans.index;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,5 +14,29 @@ import javax.inject.Named;
  */
 @Named
 public class CategoriesBean {
+    private List<String> categoryImages = new ArrayList<String>();
 
+    public List<String> getCategoryImages() {
+        return categoryImages;
+    }
+
+    @PostConstruct
+    void init(){
+        this.categoryImages.add("/resources/images/bus.png");
+        this.categoryImages.add("/resources/images/globe.png");
+        this.categoryImages.add("/resources/images/apple.png");
+        this.categoryImages.add("/resources/images/bmw.png");
+        this.categoryImages.add("/resources/images/bear_user.png");
+        this.categoryImages.add("/resources/images/ipod_noir.png");
+        this.categoryImages.add("/resources/images/apple_tv_logo.png");
+        this.categoryImages.add("/resources/images/dvd.png");
+        this.categoryImages.add("/resources/images/documents.png");
+        this.categoryImages.add("/resources/images/fish.png");
+        this.categoryImages.add("/resources/images/help.png");
+        this.categoryImages.add("/resources/images/grumpy_bear.png");
+    }
+
+    public void setCategoryImages(List<String> categoryImages) {
+        this.categoryImages = categoryImages;
+    }
 }
